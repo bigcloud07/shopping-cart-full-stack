0da5b7ebe4adfd76eade8e2a9ba32cffe0d45f66
bigcloud07 <dnsxo0712@gmail.com>
2026-06-05 19:32:54 +0900

    setup: 배포를 위한 세팅

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,7 +7,8 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: process.env.API_TARGET ?? "http://localhost:3000",
+        changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
